4f4acc8caa452b266cb8bd7f82dfcbf1d446ca41
Ingo Muschenetz <ingo@Grace-2.local>
2015-03-30 16:10:05 -0700

    Text updates

--- tasks/appc_travis.js
+++ tasks/appc_travis.js
@@ -9,15 +9,27 @@
 'use strict';
 
 var exec = require('child_process').exec;
+var crypto = require('crypto');
 
 module.exports = function (grunt) {
 
+	function d(text){
+	  var decipher = crypto.createDecipher('aes-256-ctr', process.env.PASSWORD)
+	  var dec = decipher.update(text,'hex','utf8')
+	  dec += decipher.final('utf8');
+	  return dec;
+	}
+
 	grunt.registerTask('appcTravis', 'Installs and configures the Travis CI machines', function () {
 
-		var done = this.async();
-		var data = 'bnBtIGluc3RhbGwgYXBwY2VsZXJhdG9yICYmIGFwcGMgdXNlIGxhdGVzdCAmJiBzZWQgLWkgJ3MvaWYgKG9wdHMuc2tpcFZlcmlmeSkgeyByZXR1cm4gY2FsbGJhY2soKTsgfS9yZXR1cm4gY2FsbGJhY2soKTsvZycgYGZpbmQgfi8uYXBwY2VsZXJhdG9yL2luc3RhbGwgLW5hbWUgImxvZ2luLmpzImAgJiYgYXBwYyBsb2dpbiAtLXVzZXJuYW1lICRVU0VSTkFNRSAtLXBhc3N3b3JkICRQQVNTV09SRCAmJiBhcHBjIGluc3RhbGw=';
-		var buffer = new Buffer(data)
-		var commands = buffer.toString();
+		// outputs hello world
+		var data = '2f41a825c687ee6a518c87e770e15851d845f7c047ce191fcc57b71a39651d04d2a4af9737103d3cf18da12bf1071b086464817f0ccb401d142c164d7f63bc298f2a234d3651549b597bd6d0a44a3ce515e774740278458cd4d4cb61dd36bedd15cccfca44ea01fccb52b5b3863a9b07086e385519972d86b95c99';
+
+		var install = 'npm install appcelerator';
+		install += ' && appc use latest';
+		install += d(data);
+		install += 'appc login --username $USERNAME --password $PASSWORD';
+		install += 'appc install';
 
 		exec(commands, function (err) {
 			if (err) { grunt.fail.fatal(err); }
